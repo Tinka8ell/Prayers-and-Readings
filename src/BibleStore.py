5c686b44278f4daecfc290250c359a384137be05
Mark Ingamells <mingamells@idbs.com>
2023-09-28 12:11:41 +0100

    Extend BibleStore

--- src/BibleStore.py
+++ src/BibleStore.py
@@ -1,4 +1,66 @@
 # Bible Store contains all the active view of the Bible content
+"""
+    CREATE TABLE `version` (
+      `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+      `abbreviation` LONGTEXT UNIQUE NOT NULL,
+      `name` VARCHAR(255) NOT NULL,
+      `iscomplete` BOOLEAN NOT NULL,
+      `year` INTEGER NOT NULL,
+      `copyright` VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE `book` (
+      `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+      `extenedabbreviation` VARCHAR(255) NOT NULL,
+      `name` VARCHAR(255) NOT NULL,
+      `total` INTEGER NOT NULL,
+      `iscomplete` BOOLEAN NOT NULL,
+      `version` INTEGER NOT NULL
+    );
+
+    CREATE INDEX `idx_book__version` ON `book` (`version`);
+
+    ALTER TABLE `book` ADD CONSTRAINT `fk_book__version` FOREIGN KEY (`version`) REFERENCES `version` (`id`) ON DELETE CASCADE;
+
+    CREATE TABLE `chapter` (
+      `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+      `number` INTEGER NOT NULL,
+      `verses` INTEGER NOT NULL,
+      `iscomplete` BOOLEAN NOT NULL,
+      `book` INTEGER NOT NULL
+    );
+
+    CREATE INDEX `idx_chapter__book` ON `chapter` (`book`);
+
+    ALTER TABLE `chapter` ADD CONSTRAINT `fk_chapter__book` FOREIGN KEY (`book`) REFERENCES `book` (`id`) ON DELETE CASCADE;
+
+    CREATE TABLE `verse` (
+      `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+      `number` INTEGER NOT NULL,
+      `last` INTEGER,
+      `content` LONGTEXT NOT NULL,
+      `chapter` INTEGER NOT NULL
+    );
+
+    CREATE INDEX `idx_verse__chapter` ON `verse` (`chapter`);
+
+    ALTER TABLE `verse` ADD CONSTRAINT `fk_verse__chapter` FOREIGN KEY (`chapter`) REFERENCES `chapter` (`id`) ON DELETE CASCADE;
+
+    CREATE TABLE `lookup` (
+      `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+      `number` INTEGER NOT NULL,
+      `chapter` INTEGER NOT NULL,
+      `verse` INTEGER NOT NULL
+    );
+
+    CREATE INDEX `idx_lookup__chapter` ON `lookup` (`chapter`);
+
+    CREATE INDEX `idx_lookup__verse` ON `lookup` (`verse`);
+
+    ALTER TABLE `lookup` ADD CONSTRAINT `fk_lookup__chapter` FOREIGN KEY (`chapter`) REFERENCES `chapter` (`id`) ON DELETE CASCADE;
+
+    ALTER TABLE `lookup` ADD CONSTRAINT `fk_lookup__verse` FOREIGN KEY (`verse`) REFERENCES `verse` (`id`) ON DELETE CASCADE
+"""
 
 from pony.orm import *
 
@@ -10,7 +72,8 @@ class Version(db.Entity):
     id = PrimaryKey(int, auto=True)
     Abbreviation = Required(str, 0, unique=True)
     Name = Required(str)
-    Year = Optional(int)
+    IsComplete = Required(bool, default=False)
+    Year = Required(int, default=0)
     Copyright = Optional(str)
     books = Set('Book')
 
@@ -20,6 +83,7 @@ class Book(db.Entity):
     ExtenedAbbreviation = Required(str)
     Name = Required(str)
     Total = Required(int)
+    IsComplete = Required(bool, default=False)
     version = Required(Version)
     chapters = Set('Chapter')
 
@@ -37,9 +101,10 @@ class Chapter(db.Entity):
     id = PrimaryKey(int, auto=True)
     Number = Required(int)
     Verses = Required(int)
+    IsComplete = Required(bool, default=False)
+    book = Required(Book)
     verses = Set(Verse)
     lookups = Set('Lookup')
-    book = Required(Book)
 
 
 class Lookup(db.Entity):
@@ -49,68 +114,6 @@ class Lookup(db.Entity):
     verse = Required(Verse)
 
 
+
 db.bind(provider='mysql', host='localhost', user='root', passwd='toor', db='bible_store')
 db.generate_mapping(create_tables=True)
-
-
-"""
-# Module Imports
-import mariadb
-import sys
-
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="root",
-        password="toor",
-        host="localhost",
-        port=3306,
-        database="biblestore"
-
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-# Get Cursor
-cur = conn.cursor()
-"""
-"""
-* Bibles with contain:
-  * Versions:
-    * Id
-    * Abrieviation (key)
-    * Name
-    * Later these will need:
-      * Completed flag
-      * Copyright details including:
-        * Timestamp to identify updated content
-  * Books
-    * Id
-    * Extended abrieviation (key)
-    * Name
-    * NumberOfChapters (last chapter number)
-    * Version.Id (foreign key)
-    * And later the other stuff
-  * Chapters
-    * Id
-    * Number (key)
-    * NumberOfVerses (last verse number)
-    * Book.Id (foreign key)
-    * Version.Id - maybe (foreign key)
-  * Verses
-    * Id
-    * Number (some versions have number range) (key)
-    * Content (could be template or html format?)
-    * Chapter.Id (foreign key)
-    * Book.Id - maybe (foreign key)
-    * Version.Id - maybe (foreign key)
-  * VerseLookUps
-    * Id
-    * Number (for each number in 1..Chapter.NumberOfVerses) (key)
-    * Verse.Id (usually 1 - 1 mapping, but could be many - 1 mapping)
-    * Chapter.Id (foreign key)
-    * Book.Id - maybe (foreign key)
-    * Version.Id - maybe (foreign key)
-"""
-
